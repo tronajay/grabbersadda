4814462f39fc53a57c64e59ad7e4d7aac73d9c6e
Ajay Patel <ajpatel762@gmail.com>
2021-09-22 01:11:13 +0530

    Disabled Pwa for sometime

--- PirateDeals/settings.py
+++ PirateDeals/settings.py
@@ -196,35 +196,35 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/google-login'
 LOGOUT_REDIRECT_URL = '/'
 
-# Progressive Web App Code Starts Here
-PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR,'static/js','serviceworker.js')
+# # Progressive Web App Code Starts Here
+# PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR,'static/js','serviceworker.js')
 
-PWA_APP_NAME = 'GrabbersAdda'
-PWA_APP_DESCRIPTION = "Latest Shopping Deals and Offers"
-PWA_APP_THEME_COLOR = '#00cd69'
-PWA_APP_BACKGROUND_COLOR = '#ffffff'
-PWA_APP_DISPLAY = 'standalone'
-PWA_APP_SCOPE = '/'
-PWA_APP_ORIENTATION = 'any'
-PWA_APP_START_URL = '/'
-PWA_APP_STATUS_BAR_COLOR = '#00cd69'
-PWA_APP_ICONS = [
-	{
-		'src': 'media/site_images/siteicon.png',
-		'sizes': '160x160'
-	}
-]
-PWA_APP_ICONS_APPLE = [
-	{
-		'src': 'static/site_images/siteicon.png',
-		'sizes': '160x160'
-	}
-]
-PWA_APP_SPLASH_SCREEN = [
-	{
-		'src': 'static/site_images/siteicon.png',
-		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
-	}
-]
-PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
+# PWA_APP_NAME = 'GrabbersAdda'
+# PWA_APP_DESCRIPTION = "Latest Shopping Deals and Offers"
+# PWA_APP_THEME_COLOR = '#00cd69'
+# PWA_APP_BACKGROUND_COLOR = '#ffffff'
+# PWA_APP_DISPLAY = 'standalone'
+# PWA_APP_SCOPE = '/'
+# PWA_APP_ORIENTATION = 'any'
+# PWA_APP_START_URL = '/'
+# PWA_APP_STATUS_BAR_COLOR = '#00cd69'
+# PWA_APP_ICONS = [
+# 	{
+# 		'src': 'media/site_images/siteicon.png',
+# 		'sizes': '160x160'
+# 	}
+# ]
+# PWA_APP_ICONS_APPLE = [
+# 	{
+# 		'src': 'static/site_images/siteicon.png',
+# 		'sizes': '160x160'
+# 	}
+# ]
+# PWA_APP_SPLASH_SCREEN = [
+# 	{
+# 		'src': 'static/site_images/siteicon.png',
+# 		'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+# 	}
+# ]
+# PWA_APP_DIR = 'ltr'
+# PWA_APP_LANG = 'en-US'
